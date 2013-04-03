fb558fa37c944348396a21a5a49c9957f8bd90d4
Eric Spett <eric.spett@gmail.com>
2013-04-03 18:16:22 -0400

    update routes and navigation

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,12 @@ HabitWatch::Application.routes.draw do
   root :to => "home#index"
   devise_for :users
   resources :users
+
+  devise_scope :user do
+    get '/signup' => 'devise/registrations#new', :as => 'signup'
+    get '/login' => 'devise/sessions#new', :as => 'login'
+    delete '/logout' => 'devise/sessions#destroy', :as => 'logout'
+    get '/settings' => 'devise/registrations#edit', :as => 'settings'
+  end
+
 end
