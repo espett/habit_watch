de7e9c0f17924b5e6cea673ad5ae9ac03406d63e
Eric Spett <eric.spett@gmail.com>
2013-04-03 17:02:23 -0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HabitWatch::Application.config.secret_token = '50c6311e7495c532c788393a017b553eddd44bda6407bfca4a22b34b5c8ed50e064442d57f6d63f0176a3f2138b1197494ad4a1eae1bd9ae4b5fb2d7b14499d7'
