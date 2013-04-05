9aa87467a73deb79601d20bc66a9a3685955785e
Billy Boozer <billy@livrandom.com>
2013-04-04 21:44:59 -0500

    changes to the schema upon migrate

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,7 @@ ActiveRecord::Schema.define(:version => 20130404002341) do
 
   create_table "counts", :force => true do |t|
     t.integer  "habit_id"
+    t.integer  "time"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
@@ -22,11 +23,8 @@ ActiveRecord::Schema.define(:version => 20130404002341) do
   create_table "habits", :force => true do |t|
     t.integer  "user_id"
     t.string   "name"
-    t.text     "description"
-    t.string   "type"
-    t.integer  "goal"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "roles", :force => true do |t|
